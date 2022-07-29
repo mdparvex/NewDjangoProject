7a7df8b3994aa2ff81f7f872fd0e068b2daebb05
mdparvex <mdparvex443@gmail.com>
2022-07-29 12:52:15 +0600

    added task

--- helloWorld/task/urls.py
+++ helloWorld/task/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = "task"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('add/', views.add, name="add")
     
 ]
--- helloWorld/task/views.py
+++ helloWorld/task/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django import forms
 
 # Create your views here.
-tasks=['foo', 'bar', 'bazz']
+
+class CreateNewFom(forms.Form):
+	task= forms.CharField(label = "New Task")
 
 def index(request):
-	contex = {
-		"tasks": tasks
+	
+	if "tasks" not in request.session:
+		request.session["tasks"]=[]
+
+	
+	context = {
+		"tasks": request.session["tasks"]
 	}
-	return render(request, "task/index.html", contex)
+	return render(request, "task/index.html", context)
+
+def add(request):
+	if request.method=="POST":
+		form = CreateNewFom(request.POST)
+		if form.is_valid():
+			task = form.cleaned_data["task"]
+			request.session["tasks"] +=[task]
+			return HttpResponseRedirect(reverse("task:index"))
+		else:
+			return render(request, "task/add.html", {"form": form})
+	context={
+		"form": CreateNewFom()
+	}
+	return render(request, "task/add.html", context)
